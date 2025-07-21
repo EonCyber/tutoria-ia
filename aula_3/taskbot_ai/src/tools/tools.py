cfecd10319b57fe1e7b1ccd0d5d4b32f01e47876
Ian Vieira Salgado <vieira4ev@gmail.com>
2025-07-21 08:56:51 -0300

    finalizado aula 3

--- aula_3/taskbot_ai/src/tools/tools.py
+++ aula_3/taskbot_ai/src/tools/tools.py
@@ -7,7 +7,7 @@ def make_create_task_tool(google_tasks: GoogleTasks):
     """
     def create_google_task(title: str, due: str) -> str:
         """
-          Cria uma tarefa no Google Tasks e envia uma mensagem de confirmação via Telegram.
+          Cria uma tarefa no Google Tasks. Devolve uma mensagem de sucesso.
           - title: Título da tarefa a ser criada.
           - due: Data de vencimento da tarefa no formato ISO 8601 (ex: '2023-10-31T12:00:00Z').
         """
@@ -17,5 +17,8 @@ def make_create_task_tool(google_tasks: GoogleTasks):
     return create_google_task
 
 def get_today_date() -> str:
-    """Retorna a data atual no formato ISO 8601 (UTC), ex: 2025-07-07T00:00:00Z, útil para saber o dia de hoje e qual data usar para criar tarefas."""
+    """
+        Retorna a data atual no formato ISO 8601 (UTC), 
+        ex: 2025-07-07T00:00:00Z, útil para saber o dia de hoje e qual data usar para criar tarefas.
+    """
     return datetime.now().isoformat() + "Z"
